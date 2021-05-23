b4d278be597ec1b88781e230caa8a4cd4307ed42
ReyhanPatria <reyhan2patria@gmail.com>
2021-05-24 02:34:30 +0700

    Added temporary code for testing purposes

--- app/src/main/java/com/example/kamibisa/ui/view/activity/HomeActivity.java
+++ app/src/main/java/com/example/kamibisa/ui/view/activity/HomeActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.example.kamibisa.R;
+import com.example.kamibisa.data.database.Database;
+import com.example.kamibisa.data.database.dao.UserDao;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
@@ -15,7 +17,25 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+
+        // TODO
+        // Show current user's email for testing purposes
         TextView centerText = findViewById(R.id.tv_center);
-        centerText.setText(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+        centerText.setText(
+                Database.getInstance()
+                        .getUserDao()
+                        .getFirebaseAuth()
+                        .getCurrentUser()
+                        .getEmail()
+        );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // TODO
+        // Signs out user for testing purposes
+        Database.getInstance().getUserDao().getFirebaseAuth().signOut();
     }
 }
